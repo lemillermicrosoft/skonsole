bb91b57533d2af738a4d422b58ffd562a34eb244
Lee Miller <lemiller@microsoft.com>
2023-08-15 09:46:01 -0700

    🚀 Add KernelProvider and configuration utilities
    Implement a new KernelProvider class to manage the creation and
    configuration of the SemanticKernel. Add utility classes for handling
    configuration variables and logging setup. This improves the overall
    structure and maintainability of the code.

---
+++ apps/SKonsole/Utils/Logging.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace SKonsole.Utils;
+
+internal static class Logging
+{
+    internal static ILoggerFactory GetFactory()
+    {
+        return LoggerFactory.Create(builder =>
+            {
+                builder
+                    .AddFilter("Microsoft", LogLevel.Error)
+                    .AddFilter("System", LogLevel.Error)
+                    .AddFilter("Program", LogLevel.Information)
+                    .AddConsole();
+            });
+    }
+}
