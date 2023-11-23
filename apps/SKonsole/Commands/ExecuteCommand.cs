fe40db687d9f1729ec2d17de759e85ab9c0fd200
xbotter <xbotter@live.cn>
2023-11-23 19:12:06 +0800

    ✨ Add ExecuteCommand to SKonsole
    Add a new command called ExecuteCommand to SKonsole. This command allows users to execute semantic functions directly. The command takes a prompt argument and an optional template option. The template option specifies the template to use for the semantic function, with a default value of "{{$input}}". The command uses the KernelProvider to get an instance of the semantic kernel and creates a semantic function based on the template. It then runs the semantic function with the provided prompt and outputs the result. The result is printed to the console.

---
+++ apps/SKonsole/Commands/ExecuteCommand.cs
@@ -0,0 +1,61 @@
+﻿using System.CommandLine;
+using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel;
+using SKonsole.Utils;
+
+namespace SKonsole.Commands;
+internal sealed class ExecuteCommand : Command
+{
+    private ILogger _logger;
+
+    public ExecuteCommand(ConfigurationProvider config, ILogger? logger = null) : base("exec", "Execute semantic function directly.")
+    {
+        if (logger is null)
+        {
+            using var loggerFactory = Logging.GetFactory();
+            this._logger = loggerFactory.CreateLogger(this.GetType());
+        }
+        else
+        {
+            this._logger = logger;
+        }
+
+        var promptArgument = new Argument<string>
+            ("prompt", "An argument that is parsed as a string.");
+
+        this.AddArgument(promptArgument);
+
+        var templateOption = new Option<string>(
+                            new string[] { "--template", "-t" },
+                            () => { return "{{$input}}"; },
+                           "The template to use for the semantic function.");
+
+        this.AddOption(templateOption);
+
+        this.SetHandler(async context => await RunExecuteAsync(context.GetCancellationToken(),
+            context.BindingContext.ParseResult.GetValueForArgument<string>(promptArgument),
+            context.BindingContext.ParseResult.GetValueForOption<string>(templateOption),
+            this._logger));
+    }
+
+    private static async Task RunExecuteAsync(CancellationToken cancellationToken,
+        string prompt,
+        string? template = null,
+        ILogger? logger = null)
+    {
+        var kernel = KernelProvider.Instance.Get();
+
+        if (string.IsNullOrWhiteSpace(template))
+        {
+            template = "{{$input}}";
+        }
+
+        var func = kernel.CreateSemanticFunction(template);
+
+        var output = await kernel.RunAsync(prompt, func);
+
+        var result = output.GetValue<string>();
+
+        Console.WriteLine(result);
+    }
+}
